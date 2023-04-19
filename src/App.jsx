6cf93cd64b471948db77cd0483aa71df50c82f6c
Jesus <jesusg.1301.jg@gmail.com>
2023-04-19 18:45:20 -0400

    added config component to the app and removed imports from react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useReducer } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
 import { Home, Aboutme, Contact, Portfolio, Services } from "./pages";
 import { ConfigContext } from "./context/configContext";
 import { Reducer, initialState } from "./reducer/reducer";
-import {Sidebar} from "./component";
+import {ConfigComponent, Sidebar} from "./component";
 
-//borrar router dom del package json
 
 const App = () => {
   const [state, dispatch] = useReducer(Reducer, initialState);
@@ -15,6 +13,7 @@ const App = () => {
       <AnimatePresence>
         <motion.div className="app">
           <Sidebar />
+          <ConfigComponent />
           <Home />
           <Aboutme />
           <Contact />
