80dd90a971521ba3ec9d66bf0bccedbc5435da84
Jesus <jesusg.1301.jg@gmail.com>
2023-04-19 18:51:41 -0400

    added shadow that changes dinamically with state.color

--- src/component/IconComponent.jsx
+++ src/component/IconComponent.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { IconContext } from 'react-icons/lib'
+import { ConfigContext } from '../context/configContext'
 
 const IconComponent = ({children}) => {
+  const {state} = useContext(ConfigContext)
+  const colorstate = state.color
   return (
-    <IconContext.Provider value={{className:'global-class-name'}}>
+    <IconContext.Provider value={{className:`global-class-name shadow-${colorstate}`}}>
         {children}
     </IconContext.Provider>
   )
