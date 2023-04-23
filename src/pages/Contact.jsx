d9a3e7daf06807046671ebae744687b68382a805
Jesus <jesusg.1301.jg@gmail.com>
2023-04-23 18:16:20 -0400

    added the whole contact form including the functionality to send it to me through email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { ConfigContext } from "../context/configContext";
-import {GoLocation} from 'react-icons/go'
-import {BsTelephoneInboundFill} from 'react-icons/bs'
-import {HiOutlineMail} from 'react-icons/hi'
+import { GoLocation } from "react-icons/go";
+import { BsTelephoneInboundFill } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
 import { layout } from "../style";
+import { useForm } from "../hooks/UseForm";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
+
+  const formData = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  };
+  const { name, email, subject, message, onInputChange, onResetForm } =
+    useForm(formData);
   const { state } = useContext(ConfigContext);
-  const colorstate = state.color
+  const colorstate = state.color;
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    console.log(process.env.REACT_APP_PUBLIC_KEY)
+    emailjs
+      .sendForm(
+        proccess.env.REACT_APP_SERVICE_ID,
+        proccess.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        proccess.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result)
+          alert('email was sent succesfully');
+          onResetForm();
+        },
+        (error) => {
+          alert('something happened, please try again later')
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       {(state.page === "Contact me" || state.page === "Contáctame") && (
@@ -27,22 +63,98 @@ const Contact = () => {
 
               <div className="w-full pt-9 px-4">
                 <div className="row">
-                   <ul className="flex justify-center flex-wrap">
-                    <li className="responsive-li-contact flex flex-col items-center justify-center"><GoLocation style={{width: '60px', height: '60px'
-                    }}/>
-                    <span>Address</span>
-                    <p>Maracaibo-Zulia. Venezuela</p>
+                  <ul className="flex justify-center flex-wrap">
+                    <li className="responsive-li-contact flex gap-y-3 flex-col items-center justify-center">
+                      <GoLocation style={{ width: "60px", height: "60px" }} />
+                      <span>Address</span>
+                      <p>Maracaibo-Zulia. Venezuela</p>
                     </li>
-                    <li className="responsive-li-contact flex flex-col items-center justify-center"><BsTelephoneInboundFill style={{width: '60px', height: '60px'
-                    }} />
-                    <span>Call me on
-                      </span>
-                      <p>+58 414 617-2019</p></li>
-                    <li className="responsive-li-contact flex flex-col items-center justify-center"><HiOutlineMail style={{width: '60px', height: '60px'
-                    }}/>
-                    <span>Email</span>
-                    <p>Jesusg.1301.jg@gmail.com</p></li>
-                   </ul>
+                    <li className="responsive-li-contact flex gap-y-3 flex-col items-center justify-center">
+                      <BsTelephoneInboundFill
+                        style={{ width: "60px", height: "60px" }}
+                      />
+                      <span>Call me on</span>
+                      <p>+58 414 617-2019</p>
+                    </li>
+                    <li className="responsive-li-contact flex gap-y-3 flex-col items-center justify-center">
+                      <HiOutlineMail
+                        style={{ width: "60px", height: "60px" }}
+                      />
+                      <span>Email</span>
+                      <p>Jesusg.1301.jg@gmail.com</p>
+                    </li>
+                  </ul>
+                </div>
+                <div className="row mt-8">
+                  <form
+                    ref={form}
+                    onSubmit={handlesubmit}
+                    className="contact-form padd-15"
+                  >
+                    <div className="row flex flex-wrap">
+                      <div className="form-item col-6 padd-15">
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            className="form-control bg-primary"
+                            placeholder="Name*"
+                            name="name"
+                            value={name}
+                            onChange={onInputChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-item col-6 padd-15">
+                        <div className="form-group">
+                          <input
+                            type="email"
+                            className="form-control bg-primary"
+                            placeholder="Email*"
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="form-item col-12 padd-15">
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            className="form-control bg-primary"
+                            placeholder="Subject*"
+                            name="subject"
+                            value={subject}
+                            onChange={onInputChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="form-item col-12 padd-15">
+                        <div className="form-group">
+                          <textarea
+                            className="form-control bg-primary"
+                            placeholder="Your Message*"
+                            name="message"
+                            value={message}
+                            onChange={onInputChange}
+                          ></textarea>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-12 padd-15">
+                        <button
+                          onClick={handlesubmit}
+                          className={`btn rounded-3xl bg-${colorstate}`}
+                        >
+                          Send Message
+                        </button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
